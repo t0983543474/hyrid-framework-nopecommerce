ad164ef79e6601455afc3e117882fdea4a05c51a
Trang Le <trangle.uit@gmail.com>
2021-09-29 19:51:54 +0700

    a

--- testCase/com/nopecommerce/user/User_01_Register.java
+++ testCase/com/nopecommerce/user/User_01_Register.java
@@ -44,7 +44,6 @@ public class User_01_Register {
 		
 	  Assert.assertEquals(driver.findElement(By.cssSelector("span#ConfirmPassword-error")).getText(), "Password is required.");
 		
-		
 	  
   }
   
