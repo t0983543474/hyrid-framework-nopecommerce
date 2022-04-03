899d3002d1dc5d1bc373bf4f588cc75f208d157e
TrangLee <TrangLee@DESKTOP-F7310DR>
2022-04-03 17:00:35 +0700

    extent report v3,4,5

--- actions/common/GlobalConstants.java
+++ actions/common/GlobalConstants.java
@@ -8,5 +8,7 @@ public class GlobalConstants {
 	public final static String ADMIN_PAGE_URL = "https://admin-demo.nopcommerce.com/";
 	public final static String FILEPATH_UPLOAD  = System.getProperty("user.dir") + File.separator +  "uploadFiles" + File.separator;
 	public final static String SCREENSHOT_FOLDER =  System.getProperty("user.dir") + File.separator +  "ScreenShotImage" + File.separator;
+	public final static String PROJECT_PATH = System.getProperty("user.dir") ;
+	public final static String JAVA_VERSION = "java.version";
 
 }
--- actions/reportConfigExtentV3/ExtentManager.java
+++ actions/reportConfigExtentV3/ExtentManager.java
@@ -1,31 +1,123 @@
-package reportConfigExtentV3;
-
-import com.aventstack.extentreports.ExtentReports;
-import com.aventstack.extentreports.reporter.ExtentHtmlReporter;
-import com.aventstack.extentreports.reporter.configuration.ChartLocation;
-import com.aventstack.extentreports.reporter.configuration.Theme;
-
-public class ExtentManager {
-	private static ExtentReports extent;
-	private static String reportFileName = "ExtentReport.html";
-	private static String extentReportPath = System.getProperty("user.dir") + "/extentV3/" + reportFileName;
-
-	public static ExtentReports getInstance() {
-		if (extent == null)
-			createInstance();
-		return extent;
-	}
-
-	public static ExtentReports createInstance() {
-		ExtentHtmlReporter htmlReporter = new ExtentHtmlReporter(extentReportPath);
-		htmlReporter.config().setTestViewChartLocation(ChartLocation.TOP);
-		htmlReporter.config().setChartVisibilityOnOpen(true);
-		htmlReporter.config().setTheme(Theme.DARK);
-		htmlReporter.config().setDocumentTitle("NopCommerce HTML Report");
-		htmlReporter.config().setEncoding("utf-8");
-		htmlReporter.config().setReportName("NopCommerce HTML Report");
-		extent = new ExtentReports();
-		extent.attachReporter(htmlReporter);
-		return extent;
-	}
-}
+//package reportConfigExtentV3;
+//
+//import java.io.File;
+//
+//import org.openqa.selenium.Platform;
+//
+//import com.aventstack.extentreports.ExtentReports;
+//import com.aventstack.extentreports.reporter.ExtentHtmlReporter;
+//import com.aventstack.extentreports.reporter.configuration.ChartLocation;
+//import com.aventstack.extentreports.reporter.configuration.Theme;
+//
+//public class ExtentManager {
+//	
+//	private static ExtentReports extent;
+//	private static Platform platform;
+//	private static String reportFileName = "ExtentReport.html";
+//	private static String macPath = System.getProperty("user.dir") + "/ExtentReportV3";
+//	private static String windowsPath = System.getProperty("user.dir") + "\\ExtentReportV3";
+//	private static String macReportFileLoc = macPath + "/" + reportFileName;
+//	private static String winReportFileLoc = windowsPath + "\\" + reportFileName;
+//
+//	public static ExtentReports getInstance() {
+//		if (extent == null)
+//			createInstance();
+//		return extent;
+//	}
+//
+//	// Create an extent report instance
+//	public static ExtentReports createInstance() {
+//		platform = getCurrentPlatform();
+//		String fileName = getReportFileLocation(platform);
+//		ExtentHtmlReporter htmlReporter = new ExtentHtmlReporter(fileName);
+//		htmlReporter.config().setTestViewChartLocation(ChartLocation.TOP);
+//		htmlReporter.config().setChartVisibilityOnOpen(true);
+//		htmlReporter.config().setTheme(Theme.DARK);
+//		htmlReporter.config().setDocumentTitle("NopEcommere HTML Report");
+//		htmlReporter.config().setEncoding("utf-8");
+//		htmlReporter.config().setReportName("NopEcommere HTML Report");
+//
+//		extent = new ExtentReports();
+//		extent.attachReporter(htmlReporter);
+//
+//		return extent;
+//	}
+//
+//	// Select the extent report file location based on platform
+//	private static String getReportFileLocation(Platform platform) {
+//		String reportFileLocation = null;
+//		switch (platform) {
+//		case MAC:
+//			reportFileLocation = macReportFileLoc;
+//			createReportPath(macPath);
+//			System.out.println("ExtentReport Path for MAC: " + macPath + "\n");
+//			break;
+//		case WINDOWS:
+//			reportFileLocation = winReportFileLoc;
+//			createReportPath(windowsPath);
+//			System.out.println("ExtentReport Path for WINDOWS: " + windowsPath + "\n");
+//			break;
+//		default:
+//			System.out.println("ExtentReport path has not been set! There is a problem!\n");
+//			break;
+//		}
+//		return reportFileLocation;
+//	}
+//
+//	// Create the report path if it does not exist
+//	private static void createReportPath(String path) {
+//		File testDirectory = new File(path);
+//		if (!testDirectory.exists()) {
+//			if (testDirectory.mkdir()) {
+//				System.out.println("Directory: " + path + " is created!");
+//			} else {
+//				System.out.println("Failed to create directory: " + path);
+//			}
+//		} else {
+//			System.out.println("Directory already exists: " + path);
+//		}
+//	}
+//
+//	// Get current platform
+//	private static Platform getCurrentPlatform() {
+//		if (platform == null) {
+//			String operSys = System.getProperty("os.name").toLowerCase();
+//			if (operSys.contains("win")) {
+//				platform = Platform.WINDOWS;
+//			} else if (operSys.contains("nix") || operSys.contains("nux") || operSys.contains("aix")) {
+//				platform = Platform.LINUX;
+//			} else if (operSys.contains("mac")) {
+//				platform = Platform.MAC;
+//			}
+//		}
+//		return platform;
+//	}
+//	
+//	
+//	
+//	
+//	// Update 
+//	
+////	private static ExtentReports extent;
+////	private static String reportFileName = "ExtentReport.html";
+////	private static String extentReportPath = System.getProperty("user.dir") + "/extentV3/" + reportFileName;
+////
+////	public static ExtentReports getInstance() {
+////		if (extent == null)
+////			createInstance();
+////		return extent;
+////	}
+////
+////	public static ExtentReports createInstance() {
+////		ExtentHtmlReporter htmlReporter = new ExtentHtmlReporter(extentReportPath);
+////		htmlReporter.config().setTestViewChartLocation(ChartLocation.TOP);
+////		htmlReporter.config().setChartVisibilityOnOpen(true);
+////		htmlReporter.config().setTheme(Theme.DARK);
+////		htmlReporter.config().setDocumentTitle("NopCommerce HTML Report");
+////		htmlReporter.config().setEncoding("utf-8");
+////		htmlReporter.config().setReportName("NopCommerce HTML Report");
+////		extent = new ExtentReports();
+////		extent.attachReporter(htmlReporter);
+////		return extent;
+////	}
+//}
